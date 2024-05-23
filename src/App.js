e72b476ef42190003097d704fc82f024285440cd
Vaishnavi Bandaru <vaishnavi.bandaru@thoughtworks.com>
2024-05-23 12:19:30 +0530

    Vaishnavi | implementing basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const frens = [
+    {'name':'Saurabh', 'place':'Benglur', 'color':"Blue"},
+    {'name':'Base', 'place':'Champapet', 'color':"Magenta"},
+    {'name':'Winni', 'place':'Sanga', 'color':"Green"},
+    {'name':'Kobs', 'place':'Asan', 'color':"Purple"},
+  ];
+  const listFrens = frens.map(fren =>
+      <li style={{color:fren.color}}>
+        {fren.name}
+      </li>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <h1>Frens</h1>
+        <ul>{listFrens}</ul>
+      </>
+
   );
 }
 
